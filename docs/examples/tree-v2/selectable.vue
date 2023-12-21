f952ac4e0991d5e3bddeee76b6358a1a30fef8a1
18483650852 <103616621+18483650852@users.noreply.github.com>
2023-12-21 16:17:10 +0800

    Update selectable.vue

--- docs/examples/tree-v2/selectable.vue
+++ docs/examples/tree-v2/selectable.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-tree-v2 :data="data" :props="props" show-checkbox :height="208" />
+  <el-tree-v2 :data="data" :props="props" show-checkbox :height="2" />
 </template>
 <script lang="ts" setup>
 interface Tree {
